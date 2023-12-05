bf77be5acad9fccbaa049cee11a751e3519dabcf
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-05 09:05:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // calling a method with a return type
+            Console.WriteLine(cube(5));
+
+            cube(5); 
+
+            Console.ReadLine();
+        }
+
+        //declare a method with a return type
+        static int cube(int n)
+        {
+            int res = n * n * n;
+            return res;
+        }
+    }
+}
